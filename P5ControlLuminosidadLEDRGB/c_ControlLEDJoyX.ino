905892d275cdc9c2b996902da3512b49dfac1880
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2016-04-24 20:47:57 +0200

    Update c_ControlLEDJoyX.ino

--- P5ControlLuminosidadLEDRGB/c_ControlLEDJoyX.ino
+++ P5ControlLuminosidadLEDRGB/c_ControlLEDJoyX.ino
@@ -1,27 +1,26 @@
 /*
- Controla la lumninosidad LED con Joystick eje x
+ Controla la lumninosidad LED rojo con Joystick eje x
  Kiwibot- Jose Pujol
  */
 
 // declaramos los pines
 const int joyXPin = A4; // selecciona el pin de entrada para el joystick x
-const int ledRPin = 6;  // selecciona pin para el LED
+const int ledRPin = 6;  // selecciona pin 6 para el LED rojo
 
 // variables que almacenan los datos
 int joyXValue = 0; // variable para almacenar los valores del joystick x
-int joyXMapvalue=0;  // variable que almacena el valor del sensor mapeado 0-255
+int joyXMapvalue = 0; // variable que almacena el valor del sensor mapeado 0-255
 
 void setup() {
-  // abre el puerto serie
-  // y establece la velocidad de conexion en baudios
-  Serial.begin(9600);
+  // nada que declarar en el setup
 }
 
 void loop() {
-  // lee los valores de los sensores
+  // lee el valor del sensor
   joyXValue = analogRead(joyXPin);
+  // mapea el valor a escala 0-255
   joyXMapvalue = map(joyXValue, 0, 1023, 0, 255);
 
-  // escribimos el valor en analogico
+  // escribimos el valor en analogico en el LED
   analogWrite(ledRPin, joyXMapvalue);
 }
