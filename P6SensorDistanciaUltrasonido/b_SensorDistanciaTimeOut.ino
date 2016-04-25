7a3ecf7bc12c9289a4d5e9129ff429002803a3c4
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2016-04-25 20:42:08 +0200

    Update b_SensorDistanciaTimeOut.ino

--- P6SensorDistanciaUltrasonido/b_SensorDistanciaTimeOut.ino
+++ P6SensorDistanciaUltrasonido/b_SensorDistanciaTimeOut.ino
@@ -1,7 +1,40 @@
 /*
+ Programa para medir la distancia
+ con el sensor de ultrasonidos HC SR 04
+ e imprimirla por el monitor serie
+ Añadimos Time Out
+ Cuando d=250cm T= 14600us
+ Kiwibot- jose Pujol
+ */
+// Conexion pines del sensor de distancia
+const int triggerPin = 11; // Pin donde conectamos el emisor
+const int echoPin = 12; // Pin donde conectamos el receptor
 
-Añadir TimeOut
+// variables para almacenar datos
+int duration = 0; // Variable para almacenar el tiempo
+int distance = 0; // Variable para almacenar la distancia
 
-El ejemplo C que sea con llamada a funcion cada 100 ms con millis()
-
-*/
+void setup() {
+  Serial.begin(9600); // Abrimos el puerto serie
+  pinMode(triggerPin, OUTPUT); // Configuramos trigger como salida digital
+  pinMode(echoPin, INPUT); // Configuramos echo como entrada digital
+}
+void loop() {
+  //Inicializamos el sensor
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(5);
+  // Enviamos una señal activando la salida trigger durante 10 microsegundos
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  // Medimos el ancho del pulso, cuando la lectura sea HIGH
+  // devuelve el tiempo transcurrido en microsegundos
+  duration = pulseIn(echoPin, HIGH, 15000);
+  // Calculamos la distancia en cm
+  distance = duration * 0.01715;
+  // Enviamos los datos medidos a traves del puerto serie
+  Serial.print (distance);
+  Serial.print ("\t");
+  Serial.println (duration);
+  delay(100); // Esperamos 100ms
+}
