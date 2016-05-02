ea0fe7a363fb5fd12b943eeb59a3c179796e1622
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2016-05-02 12:57:35 +0200

    Update a_RecorridoMatrizLEDs.ino

--- P11MatrizLEDsSinLibreria/a_RecorridoMatrizLEDs.ino
+++ P11MatrizLEDsSinLibreria/a_RecorridoMatrizLEDs.ino
@@ -1,16 +1,19 @@
+
+
 /* Programa que recorre los puntos de la matriz de LEDs
  *  Control de la matriz de LEDS sin librería
  *  Jose Pujol- Kiwibot
  *  Octubre 2015
 */
 
-// variables pines
+// pines registro de desplazamiento
 const int latchPin = 6;    // pin para el latch de los 74CH495
 const int datosPin = 5;    // pin para Datos serie del 74CH495
 const int relojPin = 9;     // pin para reloj del 74CH495
 
-// variables para los puntos de la matriz
+// vector con las columnas como 1s
 byte columna[7] = {B00000010, B00000100, B00001000, B00010000, B00100000, B01000000, B10000000};
+// vector con las filas como ceros
 byte fila[5] = {B11110111, B11101111, B11011111, B10111111, B01111111};
 
 
